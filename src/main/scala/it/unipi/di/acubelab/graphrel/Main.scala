f0a8a71a4913ade187381514deebca7c69a1b230
Marco Ponza <mponza@gmail.com>
2016-07-28 19:48:34 +0200

    AllBench in Main.

--- src/main/scala/it/unipi/di/acubelab/graphrel/Main.scala
+++ src/main/scala/it/unipi/di/acubelab/graphrel/Main.scala
@@ -64,7 +64,7 @@ object Bench {
 object GridLLP {
   def main(args: Array[String]) = {
     for {
-      nLabels <- 60 to 100 by 10
+      nLabels <- 10 to 100 by 10
       maxUpdates <- 100 to 1000 by 100
     } {
       val llpJson = """{"nLabels": %d, "maxUpdates": %d}""".format(nLabels, maxUpdates)
